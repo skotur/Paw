2ee973aa288c03162ac1427f2a4358df8178196e
Sebastian Kotur <seb@curvestone.io>
2024-02-02 23:57:37 +0000

    Hidden Non Enabled Buttons

--- src/components/sidebar/more-settings.tsx
+++ src/components/sidebar/more-settings.tsx
@@ -61,36 +61,6 @@ export function MoreSettings(): JSX.Element {
                 >
                   <Menu.Item>
                     {({ active }): JSX.Element => (
-                      <MenuLink
-                        className={cn(
-                          'flex w-full cursor-not-allowed gap-3 rounded-t-md p-4 duration-200',
-                          active && 'bg-main-sidebar-background'
-                        )}
-                        href='/settings'
-                        onClick={preventBubbling()}
-                      >
-                        <HeroIcon iconName='Cog8ToothIcon' />
-                        Settings and privacy
-                      </MenuLink>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }): JSX.Element => (
-                      <MenuLink
-                        className={cn(
-                          'flex w-full cursor-not-allowed gap-3 rounded-t-md p-4 duration-200',
-                          active && 'bg-main-sidebar-background'
-                        )}
-                        href='/help-center'
-                        onClick={preventBubbling()}
-                      >
-                        <HeroIcon iconName='QuestionMarkCircleIcon' />
-                        Help center
-                      </MenuLink>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }): JSX.Element => (
                       <Button
                         className={cn(
                           'flex w-full gap-3 rounded-none rounded-b-md p-4 duration-200',
